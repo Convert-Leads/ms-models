c950b4d47470421becfcf5af27ce57ac19a710b7
Rhys Morgan <40267133+EAS-Rhys@users.noreply.github.com>
2024-01-13 02:54:03 +0000

    Create content.go

---
+++ content.go
@@ -0,0 +1,9 @@
+type Content struct {
+    gorm.Model
+    OrganisationId  int        `json:"organisation_id"`
+    ContentTypeId   int        `json:"content_type_id"`
+    AvailableDatetime *time.Time `json:"available_datetime"`
+    ExpiryDatetime  *time.Time `json:"expiry_datetime"`
+    Pinned          bool       `json:"pinned"`
+    MediaId         int        `json:"media_id"`
+}
