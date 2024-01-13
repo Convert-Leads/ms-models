a91adabc5aa3b05ec87f77e421a2d74e69cee084
Rhys Morgan <40267133+EAS-Rhys@users.noreply.github.com>
2024-01-13 02:55:31 +0000

    Create userinteractions.go

---
+++ userinteractions.go
@@ -0,0 +1,8 @@
+type UserInteractions struct {
+    gorm.Model
+    UserId              int        `json:"user_id"`
+    ContentId           int        `json:"content_id"`
+    InteractionTypeId   int        `json:"interaction_type_id"`
+    InteractionDatetime *time.Time `json:"interaction_datetime"`
+    InteractionDetails  string     `json:"interaction_details"`
+}
