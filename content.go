32b4a9bf09c15e7ed7ee56f122a5af5bddfbe7e4
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-03-02 23:07:03 +0000

    updated content struct

--- content.go
+++ content.go
@@ -12,9 +12,9 @@ type Content struct {
     AvailableDatetime  *time.Time `json:"available_datetime"`
     ExpiryDatetime     *time.Time `json:"expiry_datetime"`
     Pinned             bool       `json:"pinned"`
-    Media              Media      `gorm:"foreignKey:ContentId" json:"media"` // Association to Media with JSON annotation
+    Media              *Media      `gorm:"foreignKey:ContentId" json:"media"` // Association to Media with JSON annotation
     UserInteractions   []UserInteraction `gorm:"foreignKey:ContentId" json:"user_interactions"` // Association to UserInteraction with JSON annotation
-    DeltaContent       DeltaContent `gorm:"foreignKey:ContentId" json:"delta_content"` // Association to DeltaContent with JSON annotation
+    DeltaContent       *DeltaContent `gorm:"foreignKey:ContentId" json:"delta_content"` // Association to DeltaContent with JSON annotation
     CreatedBy  uint   `json:"created_by"`  // ID of the user who created the record
     UpdatedBy  uint   `json:"updated_by"`  
     Caption    string `json:"caption"` 
