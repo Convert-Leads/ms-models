8ca4435c23044c1d9969a958d20b97b1aeaabc45
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-03-03 20:45:55 +0000

    added User object to interactions for easier retrieval

--- userinteractions.go
+++ userinteractions.go
@@ -13,6 +13,5 @@ type UserInteraction struct {
     InteractionTypeId   uint       `json:"interaction_type_id"`
     InteractionDatetime *time.Time `json:"interaction_datetime"`
     InteractionDetails  string     `json:"interaction_details"`
-    // Including reverse association to Content is typically not necessary for JSON serialization, but can be added if needed:
-    // Content             Content    `gorm:"foreignKey:ContentId" json:"content,omitempty"` // Reverse association to Content with JSON annotation (optional)
+    User                *User       `gorm:"foreignKey:UserId" json:"user"` 
 }
