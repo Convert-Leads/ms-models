be7561a9ec20252f2d9f2e5321fb853a6cf3d14c
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-03-11 14:31:49 +0000

    removed circular reference

--- stripe_detail.go
+++ stripe_detail.go
@@ -11,6 +11,6 @@ type StripeDetail struct {
 	CustomerID     string                `json:"customer_id"` // Stripe Customer ID
 	SubscriptionID string                `json:"subscription_id"` // Stripe Subscription ID
 	Active         string                  `json:"active"` // Indicates if the subscription is active
-	Organisation   Organisation          `gorm:"foreignKey:OrganisationId"`
+	// Organisation   Organisation          `gorm:"foreignKey:OrganisationId"`
 	Payments       []PaymentTransaction  `json:"payments" gorm:"foreignKey:StripeDetailID"`
 }
