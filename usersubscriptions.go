d013c1eaa70c9b67c64c7f08eb84d7dadf12de40
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-03-11 15:10:16 +0000

    updated price option

--- usersubscriptions.go
+++ usersubscriptions.go
@@ -10,6 +10,7 @@ type UserSubscription struct {
     UserId               uint        `json:"user_id"`
     SubscriptionLevel    SubscriptionLevel `gorm:"foreignKey:SubscriptionLevelId"` 
     SubscriptionLevelId  uint        `json:"subscription_level_id"`
+    PriceOptionId        uint        `json:"-"`
     PriceOption          PriceOption `gorm:"foreignKey:PriceOptionId"`
     StartDate            *time.Time `json:"start_date"`
     EndDate              *time.Time `json:"end_date"`
