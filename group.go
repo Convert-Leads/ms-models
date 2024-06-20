59422054144a4f09f90052e85c7aa6abfa3b5fe7
Andrew Williams <apwilliams508@gmail.com>
2024-06-20 11:56:46 +0100

    updated models

--- group.go
+++ group.go
@@ -6,9 +6,11 @@ import (
 
 type Group struct {
 	gorm.Model
-	OrganisationId uint   `json:"organisation_id"`
-	Name           string `json:"name"`
-	Description    string `json:"description"`
-	Members        []User `gorm:"many2many:user_groups;" json:"members"`
-	Posts          []Post `gorm:"foreignKey:GroupId" json:"posts"`
+	OrganisationId uint          `json:"organisation_id"`
+	Name           string        `json:"name"`
+	Description    string        `json:"description"`
+	Tags           []MetadataTag `gorm:"many2many:content_tags;" json:"tags"`
+	AllowedUsers   []User        `gorm:"many2many:content_allowed_users;" json:"allowed_users"`
+	Members        []User        `gorm:"many2many:user_groups;" json:"members"`
+	Posts          []Post        `gorm:"foreignKey:GroupId" json:"posts"`
 }
--- post.go
+++ post.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
@@ -11,7 +9,6 @@ type Post struct {
 	ContentID        uint              `json:"content_id"`
 	CreatedByUserId  uint              `json:"created_by_user_id"`
 	GroupId          uint              `json:"group_id"`
-	PostedAt         *time.Time        `json:"posted_at"`
 	User             User              `gorm:"foreignKey:CreatedByUserId" json:"user"`
 	Group            Group             `gorm:"foreignKey:GroupId" json:"group"`
 	UserInteractions []UserInteraction `gorm:"polymorphic:Parent;polymorphicValue:posts" json:"user_interactions"`
