dd72383d9c86012f9e683e156b0541d75a0c89ed
Andrew Williams <apwilliams508@gmail.com>
2024-06-20 12:09:25 +0100

    Auto commit

--- group.go
+++ group.go
@@ -9,8 +9,8 @@ type Group struct {
 	OrganisationId uint          `json:"organisation_id"`
 	Name           string        `json:"name"`
 	Description    string        `json:"description"`
-	Tags           []MetadataTag `gorm:"many2many:content_tags;" json:"tags"`
-	AllowedUsers   []User        `gorm:"many2many:content_allowed_users;" json:"allowed_users"`
+	Tags           []MetadataTag `gorm:"many2many:group_tags;" json:"tags"`
+	AllowedUsers   []User        `gorm:"many2many:group_allowed_users;" json:"allowed_users"`
 	Members        []User        `gorm:"many2many:user_groups;" json:"members"`
 	Posts          []Post        `gorm:"foreignKey:GroupId" json:"posts"`
 }
--- migrate.go
+++ migrate.go
@@ -1,41 +1,41 @@
 package models
 
 import (
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 func Migrate(DB *gorm.DB) error {
-    // AutoMigrate provided models
-    if err := DB.AutoMigrate(// enumerate all models here
-        &ContentType{},
-        &Organisation{},
-        &User{},
-        &CallToAction{},
-        &Content{},
-        &Collection{},
-        &Chapter{},
-        &ChapterContent{},
-        &UserInteraction{},
-        &Media{},
-        &UserInteractionType{},
-        &ContentMetadataTag{},
-        &MetadataTag{},
-        &Role{},
-        &SubscriptionLevel{},
-        &PriceOption{},
-        &UserSubscription{},
-        &UserBookmark{},
-        &DeltaContent{},
-        &StripeDetail{},
-        &PaymentTransaction{},
-        &UserRole{},
-        &UserBookmark{},
-        &Safe{},
-        
+	// AutoMigrate provided models
+	if err := DB.AutoMigrate( // enumerate all models here
+		&ContentType{},
+		&Organisation{},
+		&User{},
+		&CallToAction{},
+		&Content{},
+		&Collection{},
+		&Chapter{},
+		&ChapterContent{},
+		&UserInteraction{},
+		&Media{},
+		&UserInteractionType{},
+		&ContentMetadataTag{},
+		&MetadataTag{},
+		&Role{},
+		&SubscriptionLevel{},
+		&PriceOption{},
+		&UserSubscription{},
+		&UserBookmark{},
+		&DeltaContent{},
+		&StripeDetail{},
+		&PaymentTransaction{},
+		&UserRole{},
+		&UserBookmark{},
+		&Safe{},
+		&Group{},
+		&Post{},
+	); err != nil {
+		return err
+	}
 
-    ); err != nil {
-        return err
-    }
-
-    return nil 
+	return nil
 }
