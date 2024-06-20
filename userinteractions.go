6a4a14be468785c9225fd6075925e5234a0d401c
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-06-20 12:06:37 +0100

    updated fk for child interactions

--- userinteractions.go
+++ userinteractions.go
@@ -15,5 +15,5 @@ type UserInteraction struct {
     User                User                `gorm:"foreignKey:UserId" json:"user"`
     ParentType          string              `json:"parent_type"`
     ParentID            uint                `json:"parent_id"`
-    ChildInteractions   []UserInteraction   `gorm:"polymorphic:Parent;polymorphicValue:user_interactions" json:"child_interactions"`
+    ChildInteractions   []UserInteraction   `gorm:"foreignKey:ParentID;constraint:OnDelete:CASCADE" json:"child_interactions"`
 }
