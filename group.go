6122adb11a10e769204c658d38f34df5e1e712d2
Andrew Williams <apwilliams508@gmail.com>
2024-06-20 11:23:20 +0100

    added community models

--- group.go
+++ group.go
@@ -6,8 +6,9 @@ import (
 
 type Group struct {
 	gorm.Model
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Members     []User `gorm:"many2many:user_groups;" json:"members"`
-	Posts       []Post `gorm:"foreignKey:GroupId" json:"posts"`
+	OrganisationId uint   `json:"organisation_id"`
+	Name           string `json:"name"`
+	Description    string `json:"description"`
+	Members        []User `gorm:"many2many:user_groups;" json:"members"`
+	Posts          []Post `gorm:"foreignKey:GroupId" json:"posts"`
 }
--- post.go
+++ post.go
@@ -8,10 +8,11 @@ import (
 
 type Post struct {
 	gorm.Model
-	ContentID       uint       `json:"content_id"`
-	CreatedByUserId uint       `json:"created_by_user_id"`
-	GroupId         uint       `json:"group_id"`
-	PostedAt        *time.Time `json:"posted_at"`
-	User            User       `gorm:"foreignKey:CreatedByUserId" json:"user"`
-	Group           Group      `gorm:"foreignKey:GroupId" json:"group"`
+	ContentID        uint              `json:"content_id"`
+	CreatedByUserId  uint              `json:"created_by_user_id"`
+	GroupId          uint              `json:"group_id"`
+	PostedAt         *time.Time        `json:"posted_at"`
+	User             User              `gorm:"foreignKey:CreatedByUserId" json:"user"`
+	Group            Group             `gorm:"foreignKey:GroupId" json:"group"`
+	UserInteractions []UserInteraction `gorm:"polymorphic:Parent;polymorphicValue:posts" json:"user_interactions"`
 }
