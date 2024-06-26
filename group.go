ff623952afe8a84d4c137830fbdb8713983871ab
Andrew Williams <apwilliams508@gmail.com>
2024-06-26 12:31:36 +0100

    Auto commit

--- group.go
+++ group.go
@@ -6,8 +6,10 @@ import (
 
 type Group struct {
 	gorm.Model
-	OrganisationId uint          `json:"organisation_id"`
-	Name           string        `json:"name"`
+	OrganisationId uint   `json:"organisation_id"`
+	Name           string `json:"name"`
+	BannerID       *uint
+	Banner         *Media        `gorm:"foreignKey:BannerID" json:"banner"`
 	Description    string        `json:"description"`
 	Tags           []MetadataTag `gorm:"many2many:group_tags;" json:"tags"`
 	AllowedUsers   []User        `gorm:"many2many:group_allowed_users;" json:"allowed_users"`
