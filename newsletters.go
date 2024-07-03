288432f7048ac5e2374aacca97864c11bf9a8eb4
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-07-03 17:06:21 +0100

    fixed poly relationships

--- newsletters.go
+++ newsletters.go
@@ -1,4 +1,3 @@
-
 package models
 
 import (
@@ -7,31 +6,32 @@ import (
 
 type NewsletterTemplate struct {
 	gorm.Model
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Elements		[]NewsletterElement `json:"elements" gorm:"foreignKey:ParentID;polymorphic:Parent;polymorphicValue:newsletter_templates"`
+	Title       string              `json:"title"`
+	Description string              `json:"description"`
+	Elements    []NewsletterElement `json:"elements" gorm:"polymorphic:Parent;polymorphicValue:NewsletterTemplates"`
 }
 
 type Newsletter struct {
 	gorm.Model
-	OrganisationID uint `json:"organisation_id"`
-	TemplateID     uint `json:"template_id"`
-	ContentID      uint `json:"content_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Elements		[]NewsletterElement `json:"elements" gorm:"foreignKey:ParentID;polymorphic:Parent;polymorphicValue:newsletters"`
+	OrganisationID uint              `json:"organisation_id"`
+	TemplateID     uint              `json:"template_id"`
+	ContentID      uint              `json:"content_id"`
+	Title          string            `json:"title"`
+	Description    string            `json:"description"`
+	Elements       []NewsletterElement `json:"elements" gorm:"polymorphic:Parent;polymorphicValue:Newsletters"`
 }
 
 type NewsletterElement struct {
 	gorm.Model
-	ParentID     uint   `json:"parent_id"`
-	ParentType   string `json:"parent_type"`
-	Type         string `json:"type"`
-	Data         string `json:"data"`
-	Order        int    `json:"order"`
-	MediaID      *uint  `json:"media_id"`
-	Media        *Media `gorm:"polymorphic:Parent;polymorphicValue:newsletter_elements;foreignKey:MediaID" json:"media"`
-	Decorations  NewsletterElementDecoration `json:"decorations" gorm:"foreignKey:NewsletterElementID"`
+	ParentID       uint                         `json:"parent_id"`
+	ParentType     string                       `json:"parent_type" gorm:"type:varchar(50);polymorphic:Parent"`
+	Type           string                       `json:"type"`
+	Data           string                       `json:"data"`
+	Order          int                          `json:"order"`
+	MediaID        *uint                        `json:"media_id"`
+	Media          *Media                       `gorm:"polymorphic:Parent;polymorphicValue:NewsletterElements;foreignKey:MediaID" json:"media"`
+	Decorations    NewsletterElementDecoration  `json:"decorations" gorm:"foreignKey:NewsletterElementID"`
+	Children       []NewsletterElement          `json:"children" gorm:"foreignKey:ParentID"` // Reference to nested elements
 }
 
 type NewsletterElementDecoration struct {
@@ -40,9 +40,8 @@ type NewsletterElementDecoration struct {
 	BgColor             string `json:"bg_color"`
 	FontSize            int    `json:"font_size"`
 	FontWeight          string `json:"font_weight"`
-	FontFamily					string `json:"font_family"`
+	FontFamily          string `json:"font_family"`
 	Alignment           string `json:"alignment"`
 	Padding             string `json:"padding"`
 	Margin              string `json:"margin"`
 }
-
