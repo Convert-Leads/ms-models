25e7804793666c55b3bd713f5d0828871e3b9544
Rhys Morgan <rhys@enterpriseautomation.co.uk>
2024-07-08 18:28:30 +0100

    streamlined content model

--- content.go
+++ content.go
@@ -7,27 +7,27 @@ import (
 
 type Content struct {
     gorm.Model
-    OrganisationId     uint       `json:"organisation_id"`
-    ContentTypeId      uint       `json:"content_type_id"`
-    ContentType        ContentType `gorm:"foreignKey:ContentTypeId" json:"content_type"`
-    AvailableDatetime  *time.Time `json:"available_datetime"`
-    ExpiryDatetime     *time.Time `json:"expiry_datetime"`
-    Pinned             bool       `json:"pinned"`
-    MediaID            *uint       
-    Media              *Media     `gorm:"polymorphic:Parent;polymorphicValue:contents;foreignKey:MediaID" json:"media"` 
-    ThumbnailID        *uint       
-    Thumbnail          *Media     `gorm:"polymorphic:Parent;polymorphicValue:contents;foreignKey:ThumbnailID" json:"thumbnail"` 
-    UserInteractions   []UserInteraction `gorm:"polymorphic:Parent;polymorphicValue:contents" json:"user_interactions"`
-    DeltaContent       *DeltaContent `gorm:"foreignKey:ContentId" json:"delta_content"` 
-    Newsletter         *Newsletter `gorm:"foreignKey:ContentID" json:"newsletter"`
-    CreatedBy          uint       `json:"created_by"`  
-    UpdatedBy          uint       `json:"updated_by"`  
-    Caption            string     `json:"caption"` 
-    CTA                *CallToAction     `json:"cta" gorm:"polymorphic:Parent;polymorphicValue:contents"`
-    Tags               []MetadataTag `gorm:"many2many:content_tags;" json:"tags"`
-    AllowedUsers       []User `gorm:"many2many:content_allowed_users;" json:"allowed_users"`
-    LikeCount          int        `json:"like_count" gorm:"-"`
-    Liked              bool       `json:"liked" gorm:"-"`
-    Bookmarked         bool       `json:"bookmarked" gorm:"-"`
-    CommentCount       int        `json:"comment_count" gorm:"-"`
+    OrganisationId     uint             `json:"-"`
+    ContentTypeId      uint             `json:"t"`
+    ContentType        ContentType      `gorm:"foreignKey:ContentTypeId" json:"ct"`
+    AvailableDatetime  *time.Time       `json:"ad,omitempty"`
+    ExpiryDatetime     *time.Time       `json:"ed,omitempty"`
+    Pinned             bool             `json:"p"`
+    MediaID            *uint            `json:"-"`
+    Media              *Media           `gorm:"polymorphic:Parent;polymorphicValue:contents;foreignKey:MediaID" json:"m,omitempty"`
+    ThumbnailID        *uint            `json:"-"`
+    Thumbnail          *Media           `gorm:"polymorphic:Parent;polymorphicValue:contents;foreignKey:ThumbnailID" json:"th,omitempty"`
+    UserInteractions   []UserInteraction `gorm:"polymorphic:Parent;polymorphicValue:contents" json:"ui,omitempty"`
+    DeltaContent       *DeltaContent    `gorm:"foreignKey:ContentId" json:"dc,omitempty"`
+    Newsletter         *Newsletter      `gorm:"foreignKey:ContentID" json:"nl,omitempty"`
+    CreatedBy          uint             `json:"cb"`
+    UpdatedBy          uint             `json:"ub"`
+    Caption            string           `json:"c,omitempty"`
+    CTA                *CallToAction    `json:"cta,omitempty" gorm:"polymorphic:Parent;polymorphicValue:contents"`
+    Tags               []MetadataTag    `gorm:"many2many:content_tags;" json:"tags,omitempty"`
+    AllowedUsers       []User           `gorm:"many2many:content_allowed_users;" json:"au,omitempty"`
+    LikeCount          int              `json:"lc" gorm:"-"`
+    Liked              bool             `json:"l" gorm:"-"`
+    Bookmarked         bool             `json:"b" gorm:"-"`
+    CommentCount       int              `json:"cc" gorm:"-"`
 }
