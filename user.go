bfbbee59fb7f184b0c90a99f80930f6478ec6c9a
Andrew Williams <apwilliams508@gmail.com>
2024-07-10 16:19:03 +0100

    ~ renamed user.Invited to user.Registered

--- user.go
+++ user.go
@@ -17,10 +17,10 @@ type User struct {
 	Contact_youtube   string         `json:"contact_youtube"`
 	Contact_instagram string         `json:"contact_instagram"`
 	Contact_linkedin  string         `json:"contact_linkedin"`
-	Invited        		bool           `json:"invited"`
+	Registered        bool           `json:"registered"`
 	Roles             []Role         `json:"roles" gorm:"many2many:user_roles;"`
 	Bookmarks         []UserBookmark `json:"bookmarks" gorm:"one2many:user_bookmarks;"`
 	Groups            []Group        `gorm:"many2many:user_groups;" json:"groups"`
 	Posts             []Post         `gorm:"foreignKey:UserId" json:"posts"`
-	ProfileImage 			*Media         `gorm:"polymorphic:Parent;polymorphicValue:users" json:"profile_image"`
+	ProfileImage      *Media         `gorm:"polymorphic:Parent;polymorphicValue:users" json:"profile_image"`
 }
