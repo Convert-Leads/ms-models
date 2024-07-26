697e74a0d13d4e5b0507109b2479c6a72f58fe63
ka1ne <emailkaine@gmail.com>
2024-07-26 10:51:07 +0100

    + interaction json fields

--- content_interaction.go
+++ content_interaction.go
@@ -2,11 +2,11 @@ package models
 
 type ContentInteraction struct {
 	QModel
-	OrganisationId         uint `json:"-"`
-	UserId                 uint `json:"-"`
-	User 								 User `json:"u"`
-	ParentType             string `json:"-"`
-	ParentID               uint `json:"-"`
-	InteractionType        string `json:"it"`
-	InteractionData				string `json:"ida"`
+	OrganisationId  uint   `json:"-"`
+	UserId          uint   `json:"user_id"`
+	User            User   `json:"u"`
+	ParentType      string `json:"parent_type"`
+	ParentID        uint   `json:"parent_id"`
+	InteractionType string `json:"it"`
+	InteractionData string `json:"ida"`
 }
