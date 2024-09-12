ddc8168f37f2d202ac7efb6399dd54ca885725c2
Stuart <stuart@enterpriseautomation.co.uk>
2024-09-12 10:29:55 +0100

    add active field to price_options

--- price_option.go
+++ price_option.go
@@ -6,9 +6,10 @@ import (
 
 type PriceOption struct {
 	gorm.Model
-	SubscriptionLevelId uint               `json:"subscription_level_id"`
-	Price          float64            `json:"price"`
-	Currency       string             `json:"currency"`
-	Duration       int                `json:"duration"`
-	DurationUnit   string             `json:"duration_unit"` 
+	SubscriptionLevelId uint    `json:"subscription_level_id"`
+	Price               float64 `json:"price"`
+	Currency            string  `json:"currency"`
+	Duration            int     `json:"duration"`
+	DurationUnit        string  `json:"duration_unit"`
+	Active              bool    `json:"active"`
 }
