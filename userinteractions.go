b7c7251f7c5e57a2395147d674c805124a6b88a2
Andrew Williams <andrew.williams@enterpriseautomation.co.uk>
2024-09-27 12:12:52 +0100

    Update userinteractions.go

--- userinteractions.go
+++ userinteractions.go
@@ -1,20 +1,19 @@
 package models
 
 import (
-	"time"
-
-	"gorm.io/gorm"
+    "gorm.io/gorm"
+    "time"
 )
 
 type UserInteraction struct {
-	gorm.Model
-	UserId              uint
-	InteractionTypeId   uint
-	InteractionType     UserInteractionType `gorm:"foreignKey:InteractionTypeId" json:"interaction_type"`
-	InteractionDatetime *time.Time          `json:"interaction_datetime"`
-	InteractionDetails  string              `json:"interaction_details"`
-	User                User                `gorm:"foreignKey:UserId" json:"user"`
-	ParentType          string              `json:"parent_type"`
-	ParentID            uint                `json:"parent_id"`
-	ChildInteractions   []UserInteraction   `gorm:"foreignKey:ParentID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"child_interactions"`
+    gorm.Model
+    UserId              uint
+    InteractionTypeId   uint
+    InteractionType     UserInteractionType `gorm:"foreignKey:InteractionTypeId" json:"interaction_type"`
+    InteractionDatetime *time.Time          `json:"interaction_datetime"`
+    InteractionDetails  string              `json:"interaction_details"`
+    User                User                `gorm:"foreignKey:UserId" json:"user"`
+    ParentType          string              `json:"parent_type"`
+    ParentID            uint                `json:"parent_id"`
+    ChildInteractions   []UserInteraction   `gorm:"foreignKey:ParentID;constraint:ParentType='UserInteractions'" json:"child_interactions"`
 }
