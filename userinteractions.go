09940ba67e32cc2383a9e0fc935b46815e65fb32
Nathan Cook <nathan@Nathans-MBP.client.easlab.co.uk>
2024-10-09 11:58:57 +0100

    updated some fields to use text instead of varchar

--- userinteractions.go
+++ userinteractions.go
@@ -1,19 +1,20 @@
 package models
 
 import (
-    "gorm.io/gorm"
-    "time"
+	"time"
+
+	"gorm.io/gorm"
 )
 
 type UserInteraction struct {
-    gorm.Model
-    UserId              uint
-    InteractionTypeId   uint
-    InteractionType     UserInteractionType `gorm:"foreignKey:InteractionTypeId" json:"interaction_type"`
-    InteractionDatetime *time.Time          `json:"interaction_datetime"`
-    InteractionDetails  string              `json:"interaction_details"`
-    User                User                `gorm:"foreignKey:UserId" json:"user"`
-    ParentType          string              `json:"parent_type"`
-    ParentID            uint                `json:"parent_id"`
-    ChildInteractions   []UserInteraction   `gorm:"foreignKey:ParentID;constraint:ParentType='UserInteractions'" json:"child_interactions"`
+	gorm.Model
+	UserId              uint
+	InteractionTypeId   uint
+	InteractionType     UserInteractionType `gorm:"foreignKey:InteractionTypeId" json:"interaction_type"`
+	InteractionDatetime *time.Time          `json:"interaction_datetime"`
+	InteractionDetails  string              `json:"interaction_details" gorm:"type:text"`
+	User                User                `gorm:"foreignKey:UserId" json:"user"`
+	ParentType          string              `json:"parent_type"`
+	ParentID            uint                `json:"parent_id"`
+	ChildInteractions   []UserInteraction   `gorm:"foreignKey:ParentID;constraint:ParentType='UserInteractions'" json:"child_interactions"`
 }
