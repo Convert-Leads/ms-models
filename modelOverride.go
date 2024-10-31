9e95ac24d63e3d1414b644959e04d7e0d9fb6638
Nathan Cook <nathan@Nathans-MBP.client.easlab.co.uk>
2024-10-31 14:10:14 +0000

    update model override to use DeletedAt for the type

--- modelOverride.go
+++ modelOverride.go
@@ -1,17 +1,21 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Model struct {
-	ID        uint       `gorm:"primary_key" json:"ID"`
-	CreatedAt *time.Time `json:"created_at" swaggertype:"integer"`
-	UpdatedAt *time.Time `json:"updated_at" swaggertype:"integer"`
-	DeletedAt *time.Time `json:"-" swaggertype:"integer"`
+	ID        uint           `gorm:"primary_key" json:"ID"`
+	CreatedAt *time.Time     `json:"created_at" swaggertype:"integer"`
+	UpdatedAt *time.Time     `json:"updated_at" swaggertype:"integer"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-" swaggertype:"integer"`
 }
 
 type QModel struct {
-	ID        uint       `gorm:"primary_key" json:"ID"`
-	CreatedAt *time.Time `json:"-"`
-	UpdatedAt *time.Time `json:"-"`
-	DeletedAt *time.Time `json:"-"`
+	ID        uint           `gorm:"primary_key" json:"ID"`
+	CreatedAt *time.Time     `json:"-"`
+	UpdatedAt *time.Time     `json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
