fb782190e63edd1e3bf5205d3cea4e0ec440b78c
Nathan Cook <nathan@Nathans-MBP.client.easlab.co.uk>
2025-03-31 16:47:46 +0100

    track id of payment link

--- conversions.go
+++ conversions.go
@@ -4,10 +4,11 @@ import "gorm.io/gorm"
 
 type Conversions struct {
 	gorm.Model
-	UserID        uint  `json:"uid"` // Foreign key for User
-	Converted     bool  `json:"cnv" gorm:"default:false"`
-	PriceOptionID *uint `json:"pid"`            // Foreign key for PriceOption
-	ContentID     *uint `json:"cid,omitempty"`  // Foreign key for Content
-	CollectionID  *uint `json:"clid,omitempty"` // Foreign key for Collection
-	GroupID       *uint `json:"gid,omitempty"`  // Foreign key for Group
+	UserID        uint   `json:"uid"` // Foreign key for User
+	Converted     bool   `json:"cnv" gorm:"default:false"`
+	PriceOptionID *uint  `json:"pid"`            // Foreign key for PriceOption
+	ContentID     *uint  `json:"cid,omitempty"`  // Foreign key for Content
+	CollectionID  *uint  `json:"clid,omitempty"` // Foreign key for Collection
+	GroupID       *uint  `json:"gid,omitempty"`  // Foreign key for Group
+	PaymentID     string `json:"payid,omitempty"`
 }
